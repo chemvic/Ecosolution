81e6d00b98637f1f2a92b8828fe7c274d012a217
chemvic <chemvic79@gmail.com>
2023-12-07 02:00:06 +0200

    upd

--- src/components/SliderCard/SliderCard.jsx
+++ src/components/SliderCard/SliderCard.jsx
@@ -1,7 +1,7 @@
 
 import css from './SliderCard.module.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircumIcon from "@klarr-agency/circum-icons-react";
+import icons from '../../images/icons.svg';
+
 const SliderCard = ({photo, alt, title, description, date}) => {
    return (
      <div className={css.sliderCard}>
@@ -14,31 +14,18 @@ const SliderCard = ({photo, alt, title, description, date}) => {
               <div className={css.sliderCard__info}>
                 <div className={css.sliderCard__infoUp}>
                     <h3 className={css.sliderCard__title}>{title}</h3>
-                    <button style={{
-        backgroundColor: '#97D28B',
-        display: 'inline-flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '60px',
-        width: '60px',
-        borderRadius: '50%',
-        border: 'none',
-        cursor: 'pointer'}}  type='button' >
-            <CircumIcon name="circle_chev_right" color="#000" size="80px"/>
-                    {/* <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" color='#000' size='28px' /> */}
+                    <button  className={css.button} type='button' >
+                      <svg className={css.icon}>
+                            <use href={`${icons}#icon-arrow-right`} />
+                      </svg>
                     </button>  
                 </div>
                 <div className={css.sliderCard__infoDown}>
                     <p className={css.sliderCard__description}>{description}</p>
                     <p className={css.sliderCard__description}>{date}</p> 
                 </div>
-{/* className={css.button} */}
-
                 </div>
-                </div>
-                 
-   
-    
+                </div>            
    );
  };
  export default SliderCard;
